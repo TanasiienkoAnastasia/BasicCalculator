aa283f9fa15ef58c01c5c8f1403204f48266fcda
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-05-13 00:40:34 +0300

    add choice for function of addition

--- Main.py
+++ Main.py
@@ -13,3 +13,17 @@ def mult(a, b):
 def div(a, b):
     answer = a / b
     print(str(a) + " / " + str(b) + " = " + str(answer))
+
+print("A. Addition")
+print("B. Substraction")
+print("C. Multiplication")
+print("D. Division")
+print("E. Exit")
+
+choice = input("input your choice: ")
+
+if choice == "a" or choice == "A":
+    print("Addition")
+    a = int(input("input first number: "))
+    d = int(input("input second number: "))
+    add(a, b)
